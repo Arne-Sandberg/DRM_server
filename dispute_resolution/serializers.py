1d837ac35c36ac421685d634ee108c826cccbc15
Anatoly Bardukov <sindbag@gmail.com>
2018-07-12 18:15:09 +0300

    Add CORS

--- dispute_resolution/serializers.py
+++ dispute_resolution/serializers.py
@@ -16,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'name', 'family_name', 'email', 'info', 'judge')
-        # extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         profile_data = validated_data.pop('info')
